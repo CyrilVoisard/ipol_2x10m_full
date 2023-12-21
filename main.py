608d20f617e9447a88f743e37f2a83b6aebfba7b
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-21 20:08:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -219,6 +219,8 @@ if __name__ == "__main__":
                         help='Acquistion frequency.')
     parser.add_argument('-age', metavar='age', type=int,
                         help='Age of the subject.')
+    parser.add_argument('-distance', metavar='distance', type=int,
+                        help='Walked distance (m).')
     parser.add_argument('-min_z', metavar='min_z', type=int,
                         help='Minimum for Z-score.')
     parser.add_argument('-max_z', metavar='max_z', type=int,
